aad65fb91de5a6b0b05b45b27a5bfd83d0a798df
Jinah <monsatfri@gmail.com>
2024-02-19 20:36:34 +0900

    chore: 라이브러리 설치 및 프로젝트 설정 변경

---
+++ app/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import "../globals.css";
+import RootLayout from "./layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+}
+
+export default MyApp;
