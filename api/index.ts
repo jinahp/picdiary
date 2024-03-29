7bc7b7909dd9e93d87a5e8dffec87527747f5550
Jinah <monsatfri@gmail.com>
2024-03-29 15:47:34 +0900

    chore: Axios `interceptors` 추가
    클라이언트에서 `window.sessionStorage` 사용

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,34 @@ import axios from "axios";
 
 export const BASE_URL = "https://diary.es6.kr";
 
+const window = global.window;
+
 axios.defaults.baseURL = BASE_URL;
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Basic ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 404) {
+      return;
+    }
+    return Promise.reject(error);
+  },
+);
 
 export const diaryKeys = {
   list: () => ["diaryList"] as const,
